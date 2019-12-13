f2dcc52becf0886a409bfda72fb1aa2e9a778f2b
Philipp Rempe <philipp.rempe@gmx.net>
2019-12-13 14:46:40 +0100

    Day 5 Part 1

---
+++ 5/src/IntCode/IntCode.go
@@ -0,0 +1,97 @@
+package IntCode
+
+import (
+	"os"
+	"bufio"
+	"fmt"
+	"strconv"
+)
+
+type instruction struct {
+	size int
+	writeParams []int
+	f func(params ...int)
+}
+
+func (ins instruction) isWriteParam(i int) bool {
+	for _, a := range ins.writeParams {
+		if i == a {
+			return true
+		}
+	}
+	return false
+}
+
+type Computer struct {
+	memory []int
+	instructions map[int]instruction
+	instructionPointer int
+}
+
+func (c Computer) Run() {
+	// Parse instruction
+	// Instruction number: %100
+	for {
+		i := c.memory[c.instructionPointer] % 100
+		ins := c.instructions[i]
+		// Handle parameter modes
+		modes := c.memory[c.instructionPointer] / 100
+		params := c.memory[c.instructionPointer+1:c.instructionPointer+ins.size]
+		fmt.Println("Instruction:", i, "Params:", params, "Modes:", modes)
+		for x := 0; x < len(params); x++ {
+			m := modes % 10
+			modes /= 10
+			// Pass write params in immediate mode
+			if ins.isWriteParam(x) {
+				continue
+			}
+			switch(m) {
+			case 0: // Position Mode
+				params[x] = c.memory[params[x]]
+			case 1: // Immediate Mode
+				continue
+			default:
+				fmt.Errorf("Unexpected parameter mode: %v", m)
+			}
+		}
+		fmt.Println("With applied modes:", params)
+		ins.f(params...)
+		c.instructionPointer += ins.size
+	}
+}
+
+func NewComputer(memory []int) Computer {
+	var c Computer
+	c.memory = memory
+	c.instructions = make(map[int]instruction)
+	c.instructions = map[int]instruction{
+		1: {4, []int{2}, func(params ...int) {
+			c.memory[params[2]] = params[0] + params[1]
+		}},
+		2: {4, []int{2}, func(params ...int) {
+			c.memory[params[2]] = params[0] * params[1]
+		}},
+		3: {2, []int{0}, func(params ...int) {
+			r := bufio.NewReader(os.Stdin)
+			s, err := r.ReadString('\n')
+			if err != nil {
+				fmt.Errorf("Couldn't read from Stdin: %v\n", err)
+			}
+			s = s[:len(s)-1]
+			n, err := strconv.Atoi(s)
+			if err != nil {
+				fmt.Errorf("Couldn't convert to int: %v\n", err)
+			}
+			c.memory[params[0]] = n
+		}},
+		4: {2, []int{}, func(params ...int) {
+			fmt.Println(params[0])
+		}},
+		99: {1, []int{}, func(params ...int) {
+			// Stop computer somehow?
+			fmt.Println("HALT")
+			os.Exit(0)
+		}},
+	}
+	return c
+}
