86ff6ec9526065b4048f0859ccd950fabd59b1b2
Philipp Rempe <philipp.rempe@gmx.net>
2019-12-13 22:37:58 +0100

    Day 6 Part 1

---
+++ 6/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"bufio"
+	"strings"
+)
+
+type planet struct {
+	orbiters []*planet
+}
+
+var ctr int
+func doTheThing(p *planet, worth int) {
+	ctr += worth
+	for _, o := range p.orbiters {
+		doTheThing(o, worth+1)
+	}
+}
+
+func main() {
+	file, _ := os.Open(os.Args[1])
+	scanner := bufio.NewScanner(file)
+
+	universe := make(map[string]*planet)
+	for scanner.Scan() {
+		buf := strings.Split(scanner.Text(), ")")
+		if _, ok := universe[buf[0]]; !ok {
+			universe[buf[0]] = new(planet)
+		}
+		if _, ok := universe[buf[1]]; !ok {
+			universe[buf[1]] = new(planet)
+		}
+		universe[buf[0]].orbiters = append(universe[buf[0]].orbiters, universe[buf[1]])
+	}
+
+	doTheThing(universe["COM"], 0)
+	fmt.Println(ctr)
+}
