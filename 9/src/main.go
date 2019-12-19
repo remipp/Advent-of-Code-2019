328674e43a8c26563e954f7933119e6ef61167b6
Philipp Rempe <philipp.rempe@gmx.net>
2019-12-19 08:55:04 +0100

    Day 9 Part 1 & 2
    Had to make c.memory a pointer, maybe there's a nicer solution

---
+++ 9/src/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"IntCode"
+	"bufio"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		os.Exit(-1)
+	}
+	file, _ := os.Open(os.Args[1])
+	scanner := bufio.NewScanner(file)
+	var data []int
+	scanner.Scan()
+	buf := strings.Split(scanner.Text(), ",")
+	for _, x := range buf {
+		n, _ := strconv.Atoi(x)
+		data = append(data, n)
+	}
+	c := IntCode.NewComputer(data)
+	c.Run()
+}
